a829f64036a39e089c0886b00f22de013a84d50d
hushkmj <hushkmj@naver.com>
2024-05-28 13:33:28 +0900

    단방향 연관관계

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -9,10 +9,10 @@ public class JpaMain {
     public static void main(String[] args) {
         // 엔티티 매니저 팩토리는 웹서버가 올라오는 시점에 단 하나만 생성해서 애플리케이션 전체에 공유
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        
+
         // 엔티티 매니저는 쓰레드간에 공유 X (사용하고 버려야 한다.)
         EntityManager em = emf.createEntityManager();
-        
+
         // JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
         EntityTransaction tx = em.getTransaction();
 
@@ -21,19 +21,26 @@ public class JpaMain {
         try {
             //팀 저장
             Team team = new Team();
-             team.setName("TeamA");
-             em.persist(team);
+            team.setName("TeamA");
+            em.persist(team);
 
-             //회원 저장
+            //회원 저장
             Member member = new Member();
             member.setUsername("member1");
             member.setTeam(team);
             em.persist(member);
 
-            Member findMember = em.find(Member.class, member.getId());
-            Team findTeam = findMember.getTeam();
+            em.flush();
+            em.clear();
 
-            System.out.println("### findTeam: " + findTeam.getName());
+            Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
+
+            System.out.println("### members.size(): " + members.size());
+
+            for (Member m : members) {
+                System.out.println("### Member: " + m.getUsername());
+            }
 
             tx.commit();
 
